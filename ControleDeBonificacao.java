4fc2e5fe1050b573b52405433cf84902b08a26cd
otaviobonacina <otavio.bernado@fatec.sp.gov.br>
2022-04-25 19:21:39 +0000

    Empresa

---
+++ ControleDeBonificacao.java
@@ -0,0 +1,11 @@
+public class ControleDeBonificacao {
+    private double totalDebonus;
+
+    public double getTotalDebonus() {
+        return totalDebonus;
+    }
+
+    public void setTotalDebonus(double totalDebonus) {
+        this.totalDebonus = totalDebonus;
+    }  
+}
